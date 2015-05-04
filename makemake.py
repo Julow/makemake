70dc11e9ac25b696aeade5425cccd5a091dc0963
juloo <juloo.dsi@gmail.com>
2015-05-05 01:53:31 +0200

    Rewrite makemake (again)

--- makemake.py
+++ makemake.py
@@ -7,7 +7,7 @@
 #    By: juloo <juloo@student.42.fr>                +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2015/05/01 23:15:55 by juloo             #+#    #+#              #
-#    Updated: 2015/05/03 00:15:40 by juloo            ###   ########.fr        #
+#    Updated: 2015/05/05 01:54:12 by juloo            ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -22,7 +22,7 @@
 
 from re import compile
 from subprocess import Popen, PIPE
-from os.path import isfile
+from os import path
 from collections import OrderedDict
 from sys import argv, stdout
 from fcntl import ioctl
@@ -31,8 +31,7 @@ from termios import TIOCSTI
 variables = OrderedDict([
 	("NAME", ("", "Executable name")),
 
-	("C_DIR", (".", "Sources directory")),
-	("H_DIRS", (".", "Includes directories")),
+	("DIRS", (".", "Project directories")),
 	("O_DIR", ("o", "Obj directory")),
 
 	("LIBS", ("", "Makefiles to call")),
@@ -42,6 +41,8 @@ variables = OrderedDict([
 	("C_CC", ("clang", "C compiler")),
 	("CPP_CC", ("clang++", "Cpp compiler")),
 	("ASM_CC", ("nasm", "Asm compiler")),
+
+	("LD_CC", ("", None)),
 
 	("C_FLAGS", ("-Wall -Wextra -Werror -O2", "Clang flags")),
 	("CPP_FLAGS", ("-Wall -Wextra -Werror -O2", "Clang++ flags")),
@@ -61,69 +62,66 @@ extensions = [
 	(".s", "ASM")
 ]
 
-topTemplate = """#
-# Makemake
-#
-
-"""
-
-cRuleTemplate = """
-%(rule)s: %(source)s %(odir)s%(dependencies)s
-	@$(MSG_0) $< ; %(cc)s $(%(flags)s) $(%(heads)s) -c -o $@ $< || ($(MSG_1) $< && false)
-"""
-
-libRuleTemplate = """
-%(lib)s:
-	@make -C %(lib)s
-.PHONY: %(lib)s
-"""
-
-bodyTemplate = """
-$(NAME): $(LIBS) $(O_FILES)
-	@$(MSG_0) $@ ; ld -o $@ $(O_FILES) $(LD_FLAGS) && echo || $(MSG_1) $@
-
-$(O_DIR)/:
-	@mkdir -p $@ 2> /dev/null || true
-
-$(O_DIR)/%%:
-	@mkdir -p $@ 2> /dev/null || true
-
-clean:
-	@rm -f $(O_FILES) 2> /dev/null || true
-	@rmdir -p $(O_DIR) 2> /dev/null || true
-
-fclean: clean
-	@rm -f $(NAME) 2> /dev/null || true
-
-re: fclean all
-
-make:
-	@python %s
-
-.PHONY: all clean fclean re make
-"""
-
 regVar = compile('^\s*(\w+)\s*[:]?=\s*(.*)$')
-regInclude = compile('^\s*#\s*include\s*"([^"]+)"\s*$')
+regInclude = compile('^\s*#\s*include\s*[<"]([^">]+)[">]\s*$')
+
+class Rule():
+
+	name = None
+	dependencies = None
+	content = None
+	phony = False
+
+	def __init__(self, name, dep, content = None, phony = False):
+		self.name = name
+		self.dependencies = dep
+		self.content = content
+		self.phony = phony
+
+	def write(self, output):
+		output.write("\n%s:" % self.name)
+		for d in self.dependencies:
+			output.write(" %s" % d)
+		output.write("\n")
+		if self.content != None:
+			for l in self.content.split("\n"):
+				output.write("\t@%s\n" % l)
+		if self.phony:
+			output.write(".PHONY: %s\n" % self.name)
 
 class Makefile():
 
 	var = None
-	maxLen = 0
-	oFiles = None
-	oDirs = None
+	sources = None
+	headers = None
 
-	def __init__(self, name):
+	rules = None
+
+	def __init__(self):
 		self.var = {}
-		self.maxLen = 0
-		self.oFiles = []
-		self.oDirs = []
-		self._parse(name)
+		self.sources = {}
+		self.headers = []
+		self.rules = []
+
+#
+# Variables
+#
+
+	def parse(self, name):
+		try:
+			f = open(name, "r")
+		except:
+			return
+		for line in f:
+			m = regVar.match(line)
+			if m != None and m.group(1) in variables:
+				self.var[m.group(1)] = m.group(2)
+		f.close()
 
 	def setVar(self, name, data):
 		self.var[name] = data
 
-	def getVar(self, name, output):
+	def getVar(self, name):
 		if name in self.var:
 			return self.var[name]
 		if name in variables:
@@ -136,125 +134,167 @@ class Makefile():
 					ioctl(0, TIOCSTI, c)
 				data = raw_input()
 			data.strip()
-			if len(data) <= 0:
-				data = variables[name][0]
 			self.setVar(name, data)
-			if variables[name][1] == None:
-				output.write("\n%s := %s\n" % (name, data))
-			else:
-				output.write("\n# %s\n%s := %s\n" % (variables[name][1], name, data))
 			return data
 		return ""
 
-	def _parse(self, name):
+#
+# Files
+#
+
+	def findFiles(self):
+		cmd_line = "find %s -type f -print" % self.getVar("DIRS")
+		cmd = Popen(cmd_line.split(), stdout=PIPE)
+		for f in cmd.stdout:
+			f = path.split(f[:-1])
+			if f[1].endswith(".h") or f[1].endswith(".hpp"):
+				self.headers.append(f)
+			else:
+				e = self._extension(f)
+				if e != None:
+					self.sources[f] = (f[0], f[1][::-1].replace(e[0][::-1], "o.")[::-1])
+
+	def _extension(self, source):
+		for e in extensions:
+			if source[1].endswith(e[0]):
+				return e
+		return None
+
+	def _includes(self, source):
 		try:
-			f = open(name, "r")
+			f = open(source, "r")
 		except:
-			return
-		for line in f:
-			m = regVar.match(line)
-			if m != None and m.group(1) in variables:
-				self.setVar(m.group(1), m.group(2))
-		f.close()
-
-	def _createVariables(self, output):
-		for v in variables:
-			if v in self.var:
-				if variables[v][1] != None:
-					output.write("# %s\n" % variables[v][1])
-				else:
-					output.write("#\n")
-				output.write("%s := %s\n" % (v, self.var[v]))
-
-	def _createIntervalVars(self, output):
-		output.write("\nMSG_0 := printf '\\033[0;32m%%-%(maxlen)d.%(maxlen)ds\\033[0;0m\\r'\n" %
-			{"maxlen": self.maxLen})
-		output.write("MSG_1 := printf '\\033[0;31m%%-%(maxlen)d.%(maxlen)ds\\033[0;0m\\n'\n" %
-			{"maxlen": self.maxLen})
-		space = False
-		for v in self.var:
-			if not v in variables:
-				if not space:
-					space = True
-					output.write("\n")
-				output.write("%s := %s\n" % (v, self.var[v]))
-		output.write("\nO_FILES :=	%s\n" % " \\\n\t\t\t".join(self.oFiles))
-
-	def _dependencies(self, name, hDirs):
-		try:
-			f = open(name, "r")
-		except:
-			return ""
-		dep = ""
+			return []
+		dep = []
 		for line in f:
 			m = regInclude.match(line)
 			if m != None:
-				for d in hDirs:
-					h = "%s/%s" % (d, m.group(1))
-					if isfile(h):
-						dep += " %s" % h
+				for h in self.headers:
+					if h[1] == m.group(1):
+						dep.append("%s/%s" % h)
 		f.close()
 		return dep
 
-	def _createFileRules(self, output):
-		cmd = Popen(["find", self.getVar("C_DIR", output), "-type", "f", "-print"], stdout=PIPE)
-		for f in cmd.stdout:
-			f = f[:-1]
-			for e in extensions:
-				if f.endswith(e[0]):
-					o = f.replace(e[0], ".o")
-					varFlags = "%s_FLAGS" % e[1]
-					varHeads = "%s_HEADS" % e[1]
-					self.getVar(varFlags, output)
-					self.getVar(varHeads, output)
-					oDir = self.getVar("O_DIR", output)
-					cDir = self.getVar("C_DIR", output)
-					output.write(cRuleTemplate % {
-						"rule": o,
-						"source": f,
-						"dependencies": self._dependencies(f, self.getVar("H_DIRS", output).split()),
-						"cc": self.getVar("%s_CC" % e[1], output),
-						"flags": varFlags,
-						"heads": varHeads,
-						"odir": "%s/%s" % (oDir, "/".join(o.replace(cDir + "/", '').split('/')[:-1]))
-					})
-					if len(o) > self.maxLen:
-						self.maxLen = len(o)
-					self.oFiles.append(o)
+#
+# Build rules
+#
 
-	def _createAllRule(self, output):
-		threadsVar = self.getVar("THREADS", output)
+	def _buildRuleAll(self):
+		threads = self.getVar("THREADS")
 		try:
-			threads = int(threadsVar)
+			n = int(threads)
+			if n <= 0:
+				raise
 		except:
-			threads = 1
-			self.setVar("THREADS", 1)
-		if threads <= 1:
-			threads = 1
-			output.write("\nall: $(NAME)\n")
+			print("\033[31mWarning: $(THREADS) must be a valid positive non-null integer\033[0m")
+			n = 1
+			self.setVar("THREADS", "1")
+		if n > 1:
+			self.rules.insert(0, Rule("all", [], "make -j$(THREADS) $(NAME)", True))
 		else:
-			output.write("\nall:\n\t@make -j$(THREADS) $(NAME)\n")
+			self.rules.insert(0, Rule("all", ["$(NAME)"], None, True))
 
-	def _createLibsRules(self, output):
-		for lib in self.getVar("LIBS", output).split():
-			output.write(libRuleTemplate % {"lib": lib})
+	def _buildRuleOther(self):
+		dirs = []
+		o_dir = self.getVar("O_DIR")
+		for s in self.sources:
+			o = "%s/%s" % (o_dir, self.sources[s][0])
+			if o in dirs:
+				continue
+			dirs.append(o)
+		self.rules.append(Rule("clean", [], "rm -f $(O_FILES) 2> /dev/null || true\nrmdir -p %s $(O_DIR) 2> /dev/null || true" % " ".join(sorted(dirs, reverse=True)), True))
+		self.rules.append(Rule("fclean", ["clean"], "rm -f $(NAME)", True))
+		self.rules.append(Rule("re", ["fclean", "all"], None, True))
+		self.rules.append(Rule("make", ["fclean"], "python makemake.py", True))
 
-	def build(self, name):
+	def _buildRuleNAME(self):
+		if not "LD_CC" in self.var:
+			ld_cc = ""
+			for e in extensions:
+				if e[1] + "_CC" in self.var:
+					ld_cc = self.var[e[1] + "_CC"]
+					break
+			if ld_cc == None:
+				print("\033[31mWarning: Nothing to compile\033[0m")
+			self.setVar("LD_CC", ld_cc)
+		self.getVar("LD_FLAGS")
+		self.rules.insert(0, Rule("$(NAME)", ["$(LIBS)", "$(O_FILES)"],
+			"$(MSG_0) $@ ; $(LD_CC) -o $@ $(O_FILES) $(LD_FLAGS) && echo || $(MSG_1) $@"))
+
+	def _buildRuleSources(self):
+		o_files = []
+		o_dir = self.getVar("O_DIR")
+		for s in self.sources:
+			e = self._extension(s)
+			o = "%s/%s/%s" % (o_dir, self.sources[s][0], self.sources[s][1])
+			o_files.append(o)
+			source = "%s/%s" % s
+			dep = [source]
+			dep += self._includes(source)
+			self.rules.append(Rule(o, dep,
+				"mkdir -p %(o_dir)s 2> /dev/null || true\n$(MSG_0) $< ; %(cc)s $(%(flags)s) $(%(heads)s) -c -o $@ $< || ($(MSG_1) $< && false)" % {
+					"o_dir": "%s/%s" % (o_dir, self.sources[s][0]),
+					"cc": self.getVar("%s_CC" % e[1]),
+					"flags": "%s_FLAGS" % e[1],
+					"heads": "%s_HEADS" % e[1]
+				}))
+			self.getVar("%s_FLAGS" % e[1])
+			self.getVar("%s_HEADS" % e[1])
+		self.setVar("O_FILES", " \\\n\t".join(o_files))
+
+	def _buildRuleHeaders(self):
+		for h in self.headers:
+			header = "%s/%s" % h
+			self.rules.append(Rule(header, self._includes(header)))
+
+	def _buildRuleLibs(self):
+		for l in self.getVar("LIBS").split():
+			self.rules.append(Rule(l, [], "make -C %s" % l, True))
+
+	def build(self):
+		self._buildRuleSources()
+		self._buildRuleHeaders()
+		self._buildRuleLibs()
+		self._buildRuleNAME()
+		self._buildRuleAll()
+		self._buildRuleOther()
+
+#
+# Write Makefile
+#
+
+	def _writeVars(self, output):
+		for v in self.var:
+			output.write("\n%s := %s\n" % (v, self.var[v]))
+
+	def _writeRules(self, output):
+		for r in self.rules:
+			r.write(output)
+
+	def _writeBody(self, output):
+		m = 0
+		for r in self.rules:
+			if len(r.name) > m:
+				m = len(r.name)
+		output.write("\nMSG_0 := printf '\\033[0;32m%%-%(maxlen)d.%(maxlen)ds\\033[0;0m\\r'\n" %
+			{"maxlen": m})
+		output.write("MSG_1 := printf '\\033[0;31m%%-%(maxlen)d.%(maxlen)ds\\033[0;0m\\n'\n" %
+			{"maxlen": m})
+
+	def write(self, name):
 		try:
 			output = open(name, "w")
 		except:
 			return
-		output.write(topTemplate)
-		self._createVariables(output)
-		self.getVar("NAME", output)
-		self._createAllRule(output)
-		self._createFileRules(output)
-		self._createLibsRules(output)
-		self.getVar("LD_FLAGS", output)
-		self._createIntervalVars(output)
-		output.write(bodyTemplate % argv[0])
+		output.write("# Makemake\n")
+		self._writeVars(output)
+		self._writeBody(output)
+		self._writeRules(output)
 		output.close()
 
-makefile = Makefile("Makefile")
-makefile.build("Makefile")
-print("\033[32mMakefile ready\033[39m")
+makefile = Makefile()
+makefile.parse("Makefile")
+makefile.getVar("NAME")
+makefile.findFiles()
+makefile.build()
+makefile.write("Makefile")
