e7558785849dc60202bc0cb0a2c3ea4faa4fc4e4
jaguillo <juloo.dsi@gmail.com>
2015-09-30 11:10:23 +0200

    Column output and Parallel

--- makemake.py
+++ makemake.py
@@ -7,7 +7,7 @@
 #    By: juloo <juloo@student.42.fr>                +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2015/08/21 19:45:08 by juloo             #+#    #+#              #
-#    Updated: 2015/09/29 01:41:11 by juloo            ###   ########.fr        #
+#    Updated: 2015/09/30 11:09:46 by jaguillo         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -55,10 +55,26 @@ LIBS := %(libs)s
 
 # Compilation and linking flags
 FLAGS := -Wall -Wextra -O2
+# Same but used in debug mode
+DEBUG_FLAGS := -Wall -Wextra -g
 # Compilation flags
 HEADS := $(addprefix -I,$(DIRS))
 # Linking flags
 LINKS :=
+
+# Jobs
+JOBS := 4
+
+# Column output
+COLUMN_OUTPUT := 1
+
+ifeq ($(COLUMN_OUTPUT),0)
+	PRINT_OK = printf '\033[32m$<\033[0m\n'
+	PRINT_LINK = printf '\033[32m$@\033[0m\n'
+else
+	PRINT_OK = echo $(patsubst $(firstword $(DIRS))/%%,%%,$<) >> $(PRINT_FILE)
+	PRINT_LINK = printf '\n\033[32m$@\033[0m\n'
+endif
 
 # Objects directory
 O_DIR := o
@@ -68,20 +84,53 @@ DEPEND := depend.mk
 
 # tmp
 MODULE_RULES := $(addsuffix /.git,$(MODULES))
+PRINT_FILE := .tmp_print
+SHELL := /bin/bash
 
 # Default rule (need to be before any include)
-all: $(MODULE_RULES) libs $(NAME)
+all: $(MODULE_RULES) libs
+ifeq ($(COLUMN_OUTPUT),0)
+	make -j$(JOBS) $(NAME)
+else
+	MAX_LEN=1;															\
+	for o in $(patsubst $(O_DIR)/$(firstword $(DIRS))/%%,%%,$(O_FILES));	\
+	do																	\
+		if [[ $${#o} -gt $$MAX_LEN ]];									\
+		then															\
+			MAX_LEN=$${#o};												\
+		fi;																\
+	done;																\
+	PER_LINE=$$((`tput cols` / $$(($$MAX_LEN + 2))));					\
+	CURR=0;																\
+	rm -f $(PRINT_FILE);												\
+	touch $(PRINT_FILE);												\
+	tail -n0 -f $(PRINT_FILE) | while read l;							\
+	do																	\
+		if [[ $$CURR -ge $$PER_LINE ]];									\
+		then															\
+			CURR=0;														\
+			echo;														\
+		fi;																\
+		CURR=$$(($$CURR + 1));											\
+		printf '\033[32m%-*s\033[0m  ' $$MAX_LEN "$$l";					\
+	done &																\
+	make -j$(JOBS) $(NAME);												\
+	STATUS=$$?															\
+	kill -9 `jobs -p`;													\
+	rm -f $(PRINT_FILE)													\
+	exit $$STATUS
+endif
 
 # Include $(O_FILES) and dependencies
 -include $(DEPEND)
 
 # Linking
 $(NAME): $(LIBS_DEPEND) $(O_FILES)
-	clang $(FLAGS) -o $@ $(O_FILES) $(LINKS) && printf '\\033[32m$@\\033[0m\\n'
+	clang $(FLAGS) -o $@ $(O_FILES) $(LINKS) && $(PRINT_LINK)
 
 # Compiling
 $(O_DIR)/%%.o:
-	clang $(FLAGS) $(HEADS) -c $< -o $@ && printf '\\033[32m$<\\033[0m\\n'
+	clang $(FLAGS) $(HEADS) -c $< -o $@ && $(PRINT_OK)
 
 # Init submodules
 $(MODULE_RULES):
@@ -100,6 +149,7 @@ rebug: fclean debug
 
 # Clean obj files
 clean:
+	rm -f $(PRINT_FILE)
 	rm -f $(O_FILES)
 
 # Clean everything
@@ -108,10 +158,6 @@ fclean: clean
 
 # Clean and make
 re: fclean all
-
-# Update $(DEPEND) file
-$(DEPEND): Makefile
-	makemake || printf "\\033[31mCannot remake $(DEPEND)\\033[0m\\n"
 
 # Set debug flags
 _debug:
