f041d68671952c862c07306c4d0114093c5eb00c
jaguillo <juloo.dsi@gmail.com>
2015-11-01 10:16:31 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,34 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    setup.py                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: jaguillo <jaguillo@student.42.fr>          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2015/11/01 10:10:17 by jaguillo          #+#    #+#              #
+#    Updated: 2015/11/01 10:29:36 by jaguillo         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+import makemake
+
+try:
+	from setuptools import setup
+except ImportError:
+	from distutils.core import setup
+
+def cat_readme():
+	with open('README.md') as f:
+		return f.read()
+
+setup(
+	name='makemake',
+	version=1.0,
+	description="Makefile and dependency generator",
+	long_description=cat_readme(),
+	url='https://github.com/Julow/makemake',
+	author=makemake.__author__,
+	packages=['makemake'],
+	entry_points={'console_scripts': ['makemake2 = makemake.__main__:main']},
+	keywords=['makemake', 'makemake2'],
+)
